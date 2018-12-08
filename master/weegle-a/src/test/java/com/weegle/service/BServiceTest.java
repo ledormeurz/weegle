cb8287facd393b057049364acdbafb3de9523eee
ledormeurz <cedcori@gmail.com>
2018-12-08 19:47:06 +0100

    auto generation config file

--- master/weegle-a/src/test/java/com/weegle/service/BServiceTest.java
+++ master/weegle-a/src/test/java/com/weegle/service/BServiceTest.java
@@ -1,12 +1,51 @@
 package com.weegle.service;
 
 import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
 public class BServiceTest {
 
     @Test
-    public void getHello() {
+    public void test(){
+
+    }
+   // @Test
+    public void getHello() throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+       for(int i = 0 ; i < 30;i++){
+           executorService.submit(new Runnable() {
+               @Override
+               public void run() {
+                   RestTemplate restTemplate = new RestTemplate();
+
+                   while(true){
+                        double random = 10 * Math.random();
+                        System.out.println(random);
+                       restTemplate.getForEntity("http://localhost:32398/a",String.class);
+                       if(random<7){
+                           restTemplate.getForEntity("http://localhost:32398/something",String.class);
+                       }
+                      // try {
+                        //   Thread.sleep(1);
+                     //  } catch (InterruptedException e) {
+                    //       e.printStackTrace();
+                     //  }
+
+                   }
+
+
+               }
+           });
+       }
+
+        executorService.awaitTermination(1000000, TimeUnit.SECONDS);
     }
 }
---
+++ master/weegle-callb/src/main/java/com/weegle/service/CallBService.java
@@ -0,0 +1,28 @@
+package com.weegle.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Date;
+
+@RestController
+public class CallBService {
+
+    @Autowired
+    Environment env;
+
+    @RequestMapping("/getc")
+    public String getHello(){
+        RestTemplate restTemplate = new RestTemplate();
+        String path = "http://"+env.getProperty("WEEGLE_B_SERVICE_HOST")+":"+env.getProperty("WEEGLE_B_SERVICE_PORT")+"/gethello";
+        String returnFromB = restTemplate.getForObject(path,String.class);
+        String ret = "I m service C, its :"+new Date() +" the return from B is : "+returnFromB;
+        return ret;
+    }
+
+
+
+}
